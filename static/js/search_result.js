e6ad22041530d36f5d2c3900aaf57113dc350644
Roooooooy <royfuemail@gmail.com>
2018-12-26 20:15:00 +0800

    Add style to avaliable classroom page

--- static/js/search_result.js
+++ static/js/search_result.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
-    let classroom = "{{ url_for('reserve',room_id=room['id'],begin=begin,end=end,date=date) }}"
+    let classroom = "{{ url_for('reserve',room_id=room['id'],begin=begin,end=end,date=date)}}"
 
-    $(".classRoomButton").unbind("hover").hover(function (){
-        $(this).animate({ paddingLeft: "0px", paddingRight: "0px"  }, 300)
-        $(this).find(".nextIndicator").animate({ width: "24px" }, 300)
+    $(".classRoomButton").hover(function (){
+        $(this).removeClass("wide")
+        $(this).find(".nextIndicator").removeClass("hidden")
     },
     function (){
-        $(this).animate({ paddingLeft: "2px", paddingRight: "22px"  }, 300)
-        $(this).find(".nextIndicator").animate({ width: "0px" }, 300)
+        $(this).addClass("wide")
+        $(this).find(".nextIndicator").addClass("hidden")
+    })
+
+    $(".classRoomButton").unbind("click").click(function (){
+        window.open($(this).data("herf"), "_self");
     })
 })
